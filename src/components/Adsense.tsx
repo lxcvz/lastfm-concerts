2b4929ebcbbdbe855e9c1a07f9cc5f4f209d54c9
Lucas <lucas-mateus.dc@hotmail.com>
2024-12-07 23:39:42 -0300

    aaaaa

--- src/components/Adsense.tsx
+++ src/components/Adsense.tsx
@@ -2,11 +2,10 @@ import Script from "next/script";
 
 export function Adsense() {
   return (
-    <Script
+    <script
       async
       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7818324760901109"
       crossOrigin="anonymous"
-      strategy="lazyOnload"
-    />
+    ></script>
   );
 }
